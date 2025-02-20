3164a589121a3db2763474ec1c6491ffe6f460c3
Harsh Patel <102902757+harsh455869@users.noreply.github.com>
2025-02-20 22:38:45 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     // serverURL : "https://www.codeinfoway.com/rooted_in_love",
-    serverURL : "https://www.codeinfoway.com/rooted_in_love/"
+        serverURL : "https://certifit.in/rooted_in_love/"
+   // serverURL : "https://www.codeinfoway.com/rooted_in_love/"
 };
